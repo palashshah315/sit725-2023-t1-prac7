95e4a8a31671ac43bd1a3cb589d95f05a8303bb4
Palash <palashshah345@gmail.com>
2023-05-15 10:37:37 +1000

    Practical 7 Uploaded

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">About this kiiten</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">About this kiiten</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span><p class="card-text">' + item.description + '</p></div></div></div>';
         $('#card-section').append(itemToAppend);
     });
 }
@@ -36,13 +36,19 @@ const addProjectToApp = (project) => {
     });
 }
 
-$(document).ready(function(){
+let socket = io();
+socket.on('number', (msg) => {
+    console.log('Random Number: ' + msg);
+});
+
+
+$(document).ready(function () {
     $('.materialboxed').materialbox();
     $('.modal').modal();
 
     getProjects();
 
-    $('#formSubmit').click(()=>{
+    $('#formSubmit').click(() => {
         submitForm();
     })
 });
